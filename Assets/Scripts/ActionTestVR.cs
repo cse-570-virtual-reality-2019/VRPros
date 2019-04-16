27b85e77019e96a0ac4a9f2d38efd770a44c8866
yogesh18140 <yogesh18140@iiitd.edu.in>
2019-04-16 19:42:55 +0530

    added: VR UI test Module

--- Assets/Scripts/ActionTestVR.cs
+++ Assets/Scripts/ActionTestVR.cs
@@ -8,6 +8,8 @@ public class ActionTestVR : MonoBehaviour
     public SteamVR_Input_Sources handType; // 1
     public SteamVR_Action_Boolean teleportAction; // 2
     public SteamVR_Action_Boolean grabAction; // 3
+    public SteamVR_Action_Boolean interactUI;
+
 
 
     // Update is called once per frame
@@ -22,6 +24,9 @@ public class ActionTestVR : MonoBehaviour
         {
             print("Grab " + handType);
         }
+        if (GetInteractUI()) {
+            print("Interact UI"+handType);
+        }
     }
 
     public bool GetTeleportDown() // 1
@@ -29,6 +34,9 @@ public class ActionTestVR : MonoBehaviour
         return teleportAction.GetStateDown(handType);
     }
 
+    public bool GetInteractUI() {
+        return interactUI.GetState(handType);
+    }
     public bool GetGrab() // 2
     {
         return grabAction.GetState(handType);
